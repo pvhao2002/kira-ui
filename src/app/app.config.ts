4106e1ac9a85070bd45817b801ac125b2ba0ea98
kira.pham <kira.pham@pal.net.vn>
2025-06-27 17:52:04 +0700

    init project

---
+++ src/app/app.config.ts
@@ -0,0 +1,22 @@
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter, withHashLocation} from '@angular/router';
+
+import {routes} from './app.routes';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideClientHydration, withNoHttpTransferCache} from '@angular/platform-browser';
+import {provideHttpClient, withFetch, withInterceptorsFromDi} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideToastr} from 'ngx-toastr';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
+    provideRouter(routes, withHashLocation()),
+    provideClientHydration(withNoHttpTransferCache()),
+    provideHttpClient(withFetch(), withInterceptorsFromDi()),
+    provideAnimations(),
+    provideToastr(),
+  ]
+};
---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app.html',
+  standalone: true,
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'kira-ui';
+}
